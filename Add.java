2c21ec780123d6a515b2ae0e72377d6a2f2d0502
tinky <titantinky@gmail.com>
2022-04-27 09:13:24 +0530

    initialize

---
+++ Add.java
@@ -0,0 +1,13 @@
+class Add
+{
+public static void main(String args[])
+{
+float a=10.0f;
+int b=20;
+float Sum=0.0f;
+System.out.println("The first number is "+a);//
+System.out.println("The second number is "+b);
+Sum=a+b;
+System.out.println("Sum is "+Sum);
+}
+}
---
+++ Area.java
@@ -0,0 +1,13 @@
+class Area
+{
+public static void main(String args[])
+{
+int h=20, b=30, Area;
+System.out.println("The height of the triangle is "+h);
+System.out.println("The base of the triangle is "+b);
+Area=h*b;
+System.out.println("Area of the triangle is "+Area);
+}
+}
+
+
