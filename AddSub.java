e71276e3e54acef6eb15a565771589039c5e0e4c
tinky <titantinky@gmail.com>
2022-04-28 08:25:25 +0530

    initialize

---
+++ AddSub.java
@@ -0,0 +1,24 @@
+class AddSub
+{
+	static int a=60,b=30;
+public static void add()
+{
+//int a=20;
+//int b=50;
+int c=a+b;
+System.out.println("Sum= "+c);
+}
+public static void sub()
+{
+//int a=50;
+//int b=20;
+int c=a-b;
+System.out.println("Difference= "+c);
+}
+public static void main(String args[])
+{
+AddSub.add();
+AddSub.sub();
+}
+}
+
